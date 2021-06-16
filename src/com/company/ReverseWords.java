cc71c75d4f1fd12814d22c4ef9cf7cd2628f31a7
Iliya333 <69747853+Iliya333@users.noreply.github.com>
2021-06-16 13:56:40 +0500

    Решения codeWars

--- src/com/company/ReverseWords.java
+++ src/com/company/ReverseWords.java
@@ -1,4 +1,32 @@
 package com.company;
 
+
+
 public class ReverseWords {
+    public static void main(String args[]) {
+        System.out.println(reverseWords("ehT kciuq nworb xof spmuj revo eht yzal .god"));
+        System.out.println(reverseWords("The quick brown fox jumps over the lazy dog."));
+        System.out.println(reverseWords("  "));
+    }
+
+    public static String reverseWords(final String original) {
+          String[] words = original.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (String s : words)
+        {
+            String rev = " ";
+            for (int i = 0; i < s.length(); i++)
+            {
+                rev = s.charAt(i) + rev;
+            }
+
+            builder.append(rev);
+        }
+
+        return builder.toString().trim();
+    }
 }
+
+
+
+
